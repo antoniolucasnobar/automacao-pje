72a777aff932b797b241543842fc6d1758fbd062
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2019-10-21 17:21:59 -0300

    [RELEASE] cleaning comments

--- src/main/kotlin/com/nobar/main.kt
+++ src/main/kotlin/com/nobar/main.kt
@@ -13,24 +13,11 @@ fun main(args: Array<String>) {
 
     Utils.loadProperties()
     val ffOptions = FirefoxOptions()
-//    ffOptions.addPreference("browser.link.open_newwindow", 1)
-//    ffOptions.setCapability("marionette", true);
+    ffOptions.addPreference("browser.link.open_newwindow", 1)
+    ffOptions.setCapability("marionette", true);
 //    "C:\\Selenium\\geckodriver.exe"
     System.setProperty("webdriver.gecko.driver", Utils.getProperties("gecko"))
 
-    ffOptions.setCapability("platform", org.openqa.selenium.Platform.WINDOWS)
-    ffOptions.setCapability("browserName", "firefox")
-    ffOptions.setCapability("version", "60.0.1")
-    ffOptions.setCapability("marionette", Utils.getProperties("marionette"))
-//        var capabilities = DesiredCapabilities()
-//
-//    capabilities = DesiredCapabilities.firefox()
-//    capabilities.browserName = "firefox"
-//    capabilities.version = "60.0.1"
-//    capabilities.platform = org.openqa.selenium.Platform.WINDOWS
-//    capabilities.setCapability("marionette", false)
-//
-//    ffOptions.merge(capabilities)
     val driver = FirefoxDriver(ffOptions)
     driver?.manage()?.timeouts()?.implicitlyWait(3, TimeUnit.SECONDS)
 //    driver?.manage()?.window()?.maximize()
